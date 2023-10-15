b1096ed3ec78f04472d78f8d3b23faceafe3a081
Zrinka <zrinka.cvitanovic@estudent.hr>
2023-10-15 07:50:13 +0200

    translated

--- LeksickiAnalizator.py
+++ LeksickiAnalizator.py
@@ -1,30 +1,28 @@
-# ne koristiti regexe!
-# TODO: implementiraj zagrade!
-def isoperator(word, line_counter):
+def is_operator(word, line_counter):
     if word == "+":
-        print('OP_PLUS', line_counter, word)
+        print("OP_ADD", line_counter, word)
         return True
     if word == "-":
-        print('OP_MINUS', line_counter, word)
+        print("OP_SUB", line_counter, word)
         return True
     if word == "*":
-        print('OP_PUTA', line_counter, word)
+        print("OP_MULTIPLY", line_counter, word)
         return True
     if word == "/":
-        print('OP_DIJELI', line_counter, word)
+        print("OP_DIVIDE", line_counter, word)
         return True
     if word == "=":
-        print('OP_PRIDRUZI', line_counter, word)
+        print("OP_ASSIGN", line_counter, word)
     if word == "(":
-        print('L_ZAGRADA', line_counter, word)
+        print("LEFT_P", line_counter, word)
         return True
     if word == ")":
-        print('D_ZAGRADA', line_counter, word)
+        print("RIGHT_P", line_counter, word)
         return True
     return False
 
 
-def isvariable(word):
+def is_variable(word):
     if not word:
         return False
     if not word[0].isalpha():
@@ -37,7 +35,7 @@ def isvariable(word):
     return True
 
 
-def isconst(word):
+def is_const(word):
     if not word:
         return False
     for char in word:
@@ -52,49 +50,48 @@ def check_expression(word, line_counter):
         if character.isalpha() or str(character).isdigit():
             variable_or_const += character
         else:
-            if isvariable(variable_or_const):
-                print('IDN', line_counter, variable_or_const)
-            elif isconst(variable_or_const):
-                print('BROJ', line_counter, variable_or_const)
-            isoperator(character, line_counter)
+            if is_variable(variable_or_const):
+                print("ID", line_counter, variable_or_const)
+            elif is_const(variable_or_const):
+                print("NUM", line_counter, variable_or_const)
+            is_operator(character, line_counter)
             variable_or_const = ""
 
     if variable_or_const != "":
-        if isvariable(variable_or_const):
-            print('IDN', line_counter, variable_or_const)
-        elif isconst(variable_or_const):
-            print('BROJ', line_counter, variable_or_const)
-
+        if is_variable(variable_or_const):
+            print("ID", line_counter, variable_or_const)
+        elif is_const(variable_or_const):
+            print("NUM", line_counter, variable_or_const)
 
 
 def check_long_words(word, line_counter):
-    if word == "za":
-        print('KR_ZA', line_counter, word)
-    elif word == "az":
-        print('KR_AZ', line_counter, word)
-    elif word == "od":
-        print('KR_OD', line_counter, word)
-    elif word == "do":
-        print('KR_DO', line_counter, word)
+    if word == "for":
+        print("KW_FOR", line_counter, word)
+    elif word == "rof":
+        print("KW_ROF", line_counter, word)
+    elif word == "from":
+        print("KW_FROM", line_counter, word)
+    elif word == "to":
+        print("KW_TO", line_counter, word)
 
     # can be either a variable, a constant or an expression
     else:
         if word[0].isdigit():
-                const = ""
-                counter = 0
-                for char in word:
-                    if char.isdigit():
-                        counter += 1
-                        const += char
-                    else:
-                        print('BROJ', line_counter, const)
-                        word = word[counter::]
-                        break
-        if isvariable(word):
-            print('IDN', line_counter, word)
+            const = ""
+            counter = 0
+            for char in word:
+                if char.isdigit():
+                    counter += 1
+                    const += char
+                else:
+                    print("NUM", line_counter, const)
+                    word = word[counter::]
+                    break
+        if is_variable(word):
+            print("ID", line_counter, word)
             return
-        elif isconst(word):
-            print('BROJ', line_counter, word)
+        elif is_const(word):
+            print("NUM", line_counter, word)
             return
         else:
             check_expression(word, line_counter)
@@ -104,7 +101,7 @@ def main():
     line_counter = 0
     while True:
         try:
-            user_input = input("")
+            user_input = input()
             if not user_input:
                 line_counter += 1
                 continue
@@ -116,11 +113,12 @@ def main():
                 elif len(word) > 1:
                     check_long_words(word, line_counter)
                 else:
-                    if isconst(word):
-                        print('BROJ', line_counter, word)
-                    elif isvariable(word):
-                        print('IDN', line_counter, word)
-                    else: isoperator(word, line_counter)
+                    if is_const(word):
+                        print("NUM", line_counter, word)
+                    elif is_variable(word):
+                        print("ID", line_counter, word)
+                    else:
+                        is_operator(word, line_counter)
         except EOFError:
             break
 
